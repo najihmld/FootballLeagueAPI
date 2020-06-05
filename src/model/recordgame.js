4d6a0aa91e378bb4b46faabbbc7f48adab08d858
Najih Mld <najihmaulida@gmail.com>
2020-06-05 16:19:37 +0700

    post recordgame & update club points

--- src/model/recordgame.js
+++ src/model/recordgame.js
@@ -10,21 +10,97 @@ module.exports = {
               id: result.insertId,
               ...setData
             }
-            const clubData = {
-              clubname: setData.clubhomename,
-              points: '10'
+
+            const score = setData.score
+            const newScore = score.split(':')
+
+            function point(score1, score2) {
+              if(score1 > score2) {
+                 return 3
+            }  else if(score1 < score2) {
+                 return 0
+            } else if(score1 == score2) {
+                return 1
             }
-            connection.query(
-              'INSERT INTO leaguestanding SET ?', clubData, (err, result) => {
-                if(!err) {
-                  console.log('berhasilll')
-                } else {
-                  reject(new Error(err))
-                }
+            }
+
+            const clubHome = {
+              clubname: setData.clubhomename,
+              points: point(newScore[0], newScore[1])
+            }
+            const clubAway = {
+              clubname: setData.clubawayname,
+              points: point(newScore[1], newScore[0])
+            }
+
+            connection.query(`SELECT * FROM leaguestanding WHERE clubname LIKE '%${clubHome.clubname}%'`, (err, result) => {
+              if(result.length === 1) {
+                const newPoints = Number(result[0].points) + clubHome.points
+                connection.query(`UPDATE leaguestanding SET points='${newPoints}' WHERE clubname='${clubHome.clubname}'`, (err, result) => {
+                  if(!err) {
+                    connection.query(`SELECT * FROM leaguestanding WHERE clubname LIKE '%${clubAway.clubname}%'`, (err, result) => {
+                      if(result.length === 1) {
+                        const newPoints = Number(result[0].points) + clubAway.points
+                        connection.query(`UPDATE leaguestanding SET points='${newPoints}' WHERE clubname='${clubAway.clubname}'`, (err, result) => {
+                          if(!err) {
+                            resolve(newResult)
+                          } else{
+                            reject(new Error(errr))
+                          }
+                        })
+                      } else {
+                        connection.query(
+                          'INSERT INTO leaguestanding SET ?', clubAway, (err, result) => {
+                            if(!err) {
+                              resolve(newResult)
+                            } else {
+                              reject(new Error(err))
+                            }
+                          }
+                        )
+                      }
+                    })
+
+
+                  } else{
+                    reject(new Error(errr))
+                  }
+                })
+              } else {
+                connection.query(
+                  'INSERT INTO leaguestanding SET ?', clubHome, (err, result) => {
+                    if(!err) {
+                      connection.query(`SELECT * FROM leaguestanding WHERE clubname LIKE '%${clubAway.clubname}%'`, (err, result) => {
+                        if(result.length === 1) {
+                          const newPoints = Number(result[0].points) + clubAway.points
+                          connection.query(`UPDATE leaguestanding SET points='${newPoints}' WHERE clubname='${clubAway.clubname}'`, (err, result) => {
+                            if(!err) {
+                              resolve(newResult)
+                            } else{
+                              reject(new Error(errr))
+                            }
+                          })
+                        } else {
+                          connection.query(
+                            'INSERT INTO leaguestanding SET ?', clubAway, (err, result) => {
+                              if(!err) {
+                                resolve(newResult)
+                              } else {
+                                reject(new Error(err))
+                              }
+                            }
+                          )
+                        }
+                      })
+                    } else {
+                      reject(new Error(err))
+                    }
+                  }
+                )
               }
-            )
-            
-            resolve(newResult)
+            })
+
+           
           } else {
             reject(new Error(err))
           }
